1d52aff2e8ea3ad3d6c57cdc4c4d6a12403813d7
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-06-17 06:41:31 +0000

    0x17

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#Apache is returning a 500 error. Once you find the issue
+exec { 'killmenow':
+    command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+    path    => '/bin',
+}
